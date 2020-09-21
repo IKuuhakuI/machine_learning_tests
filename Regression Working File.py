fd9cf2ee4f109edec5ead24434ad6df7ee014a39
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-09-20 21:35:27 -0300

    Save the model

--- Regression Working File.py
+++ Regression Working File.py
@@ -4,6 +4,11 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 
+import matplotlib.pyplot as pyplot
+import pickle
+
+from matplotlib import style
+
 data = pd.read_csv("student-mat.csv", sep=';')
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
@@ -15,13 +20,19 @@ y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+# linear = linear_model.LinearRegression()
+#
+# linear.fit(x_train, y_train)
+# acc = linear.score(x_test, y_test)
+# print(acc)
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+# with open("studentmodel.pickle", "wb") as f:
+#    pickle.dump(linear, f)
 
-print("Co: \n", linear.coef_)
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print("Coefficient: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
 
 predictions = linear.predict(x_test)
